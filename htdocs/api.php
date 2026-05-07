c5a6306bcd2197711c0c6fe77772c68a57bc9e09
Srinivasa Harsha Vardhan <hydrogenchloride8@gmail.com>
2026-05-07 16:42:02 +0530

    Update api.php

--- htdocs/api.php
+++ htdocs/api.php
@@ -1,4 +1,9 @@
 <?php
+
+header('Content-Type: application/json');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);    
+
 include 'db.php';
 
 $total = $db->querySingle("SELECT COUNT(*) FROM logs");
